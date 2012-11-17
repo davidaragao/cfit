756cb34bc5cd6b86c10ca931e91efb79dcb36ef8
David Aragao <davidaragao@gmail.com>
2012-11-17 18:56:29 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cfit::Application.config.secret_token = '10bbb1ff8ce509c556a0e7d502071770feff13122d6bdddb4dd4a5c1acdc25f0b62c134f5e6f660c4e194c90b34be9da7ead8ac44db2169a75e9900a141c8230'
